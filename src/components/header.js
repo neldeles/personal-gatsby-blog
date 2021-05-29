4c31616f0fb7c7b80887a918861ee96aca8cc0d2
neldeles <neldeles@gmail.com>
2021-05-29 15:48:09 +0800

    navbar done

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,32 @@
-import * as React from "react"
-import PropTypes from "prop-types"
+/** @jsx jsx */
+import { jsx } from "@emotion/react"
+import React from "react"
 import { Link } from "gatsby"
+import tw from "twin.macro"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+const styles = {
+  logo: tw`text-3xl font-black text-black mr-auto`,
+  header: tw`items-center bg-white flex w-full max-w-5xl mx-auto py-0 px-24 h-24`,
+  links: tw`p-4 text-base font-medium text-gray-500 hover:text-gray-900`,
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
+const Header = () => {
+  return (
+    <div css={styles.header}>
+      <Link to="/" aria-label="home" css={styles.logo}>
+        neldeles
+      </Link>
+      <Link to="blog" css={styles.links}>
+        blog
+      </Link>
+      <Link to="contact" css={styles.links}>
+        contact
+      </Link>
+      <Link to="resume" css={styles.links}>
+        resume
+      </Link>
+    </div>
+  )
 }
 
 export default Header
